f039d201871802c201937d36a84a89ba9c3966c9
James Hodgkinson <james@terminaloutcomes.com>
2023-06-10 10:52:46 +1000

    shellcheck fixes for update.sh

--- bin/update.sh
+++ bin/update.sh
@@ -2,9 +2,9 @@
 
 # make sure we are root
 DIR="$(dirname "$(readlink -f "$0")")"
-echo $DIR
+echo "${DIR}"
 
-userid=`id -u`
+userid="$(id -u)"
 if [ ! "$userid" = "0" ]; then
    echo "You have to run this script as root. eg."
    echo "  sudo $0"
@@ -21,42 +21,56 @@ if [ -f /etc/dshield.ini ]; then
     inifile="/etc/dshield.ini"
 fi
 if [ -f ~/etc/dshield.ini ]; then
-    inifile="~/etc/dshield.ini"
+    inifile="$HOME/etc/dshield.ini"
 fi
 if [ -f ~/.dshield.ini ]; then
-    inifile="~/.dshield.ini"
+    inifile="$HOME/.dshield.ini"
 fi
 
 source <(grep = $inifile)
 
 if [ "$1" == "--cron" ]; then
-    if [ "$manualupdates" == 1 ]; then
-	exit
+    if [ -n "$manualupdates" ]; then
+        if [ "$manualupdates" == 1 ]; then
+        exit
+        fi
     fi
 fi
 
 
 
-if [ "$version" == "" ]; then
+if [ -z "$version" ]; then
     version=0;
 fi
 
-echo Currently configured for $email userid $userid
-echo Version installed: $version
-user=`echo $email | sed 's/+/%2b/' | sed 's/@/%40/'`
-nonce=`openssl rand -hex 10`
-hash=`echo -n $email:$apikey | openssl dgst -hmac $nonce -sha512 -hex | cut -f2 -d'=' | tr -d ' '`
-checkapikey=$(curl -s https://isc.sans.edu/api/checkapikey/$user/$nonce/$hash/$version/$piid)
-if echo $checkapikey | grep -q '<result>ok</result>'; then
+if [ -z "${email}" ]; then
+    echo "No email configured! Check ${inifile} for the 'email' entry."
+    exit 1
+fi
+if [ -z "${apikey}" ]; then
+    echo "No API key configured! Check ${inifile} for the 'apikey' entry."
+    exit 1
+fi
+if [ -z "${piid}" ]; then
+    echo "piid is not set in the config! Check ${inifile} for the 'piid' entry."
+    exit 1
+fi
+
+echo "Currently configured for $email userid $userid"
+echo "Version installed: $version"
+user=$(echo "$email" | sed 's/+/%2b/' | sed 's/@/%40/')
+nonce=$(openssl rand -hex 10)
+hash=$(echo -n "$email:$apikey" | openssl dgst -hmac "$nonce" -sha512 -hex | cut -f2 -d'=' | tr -d ' ')
+checkapikey=$(curl -s "https://isc.sans.edu/api/checkapikey/$user/$nonce/$hash/$version/$piid")
+if echo "$checkapikey" | grep -q '<result>ok</result>'; then
     echo "API Key OK"
-    newversion=$(echo $checkapikey | egrep -o '<version>[^<]+</version>'|egrep -o '[0-9]+')
+    newversion=$(echo "$checkapikey" | grep -E -o '<version>[^<]+</version>'| grep -E -o '[0-9]+')
 else
     echo "Bad API Key. check API key in /etc/dshield.ini"
-    exit
+    exit 1
 fi
-    
 
-echo Current Version: $newversion
+echo "Current Version: $newversion"
 
 if [ "$newversion" -gt "$version" ]; then
     echo "Update"
